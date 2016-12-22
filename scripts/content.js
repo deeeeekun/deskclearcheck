0c2bcb630fe8a72f7b8b228771a1c67af938e0d2
david.genesis.cruz <david.genesis.cruz@bizreach.co.jp>
2016-12-22 17:14:26 +0900

    Beautified alert

--- scripts/content.js
+++ scripts/content.js
@@ -18,7 +18,47 @@ function showAlert(isOff) {
 				return;
 		}
 
-		alert('机上をきれいにし、キャビネを施錠しましたか？');
+		var overlayId = 'deskclearcheck-overlay',
+				overlay = createOverlay(overlayId);
+		document.body.appendChild(overlay);
+
+		setTimeout(function() {
+				fadeOut(overlayId, function() {
+						overlay.remove();
+				});
+		}, 3000);
+}
+
+function createOverlay(overlayId) {
+		var overlay = document.createElement('div');
+		overlay.id = overlayId;
+		overlay.setAttribute('style', 'position: absolute; left: 0px; top: 0px; background-color: rgba(255, 255, 255, 0.5); z-index: 2001; height: 100%; width: 100%; display: flex; align-items: center;'); 
+
+		var modalDiv = document.createElement('div');
+		modalDiv.setAttribute('style', 'display: inline-block; background-color: #ffffff; border: 1px solid #002288; width: 500px; z-index: 2002; margin: 0 auto; padding: 15px; text-align: center;');
+
+		var modalText = document.createElement('p');
+		modalText.setAttribute('style', 'font-weight: bold; font-size: 20px;');
+		modalText.innerHTML = 'お疲れ様です。<br/>机上をきれいにし、キャビネを施錠しましたか？';
+		
+		modalDiv.appendChild(modalText);
+		overlay.appendChild(modalDiv);
+
+		return overlay;
+}
+
+function fadeOut(elementId, callback) {
+		var element = document.getElementById(elementId),
+				opacity = element.style.opacity || 1,
+				timer = setInterval(function() {
+						if (opacity < 0) {
+								clearInterval(timer);
+								callback();
+						}
+		
+						opacity -= 0.1;
+						element.style.opacity = opacity;
+				}, 50);
 }
 
 setTimeout(init(), 1000);
