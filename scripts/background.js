8ff7e183a3aec20b5f6b46b10ec6f3bed8ef3e05
David Genesis Cruz (timetosaygoodbye) <david.genesis.cruz@bizreach.co.jp>
2016-12-26 20:12:02 +0900

    Changed event listener target to a parent element to workaround the button element rewrite

--- scripts/background.js
+++ scripts/background.js
@@ -1,42 +1,42 @@
 var isOff = false;
 
 function toggleOnOff() {
-		isOff = !isOff;
-		
-		// default icons
-		var settings = {
-				title: {
-						title: 'リマインダー: ON'
-				},
-				icons: {
-						path: {
-								'19': 'icons/19.png',
-								'38': 'icons/38.png'
-						}
-				}
-		};
+    isOff = !isOff;
+    
+    // default icons
+    var settings = {
+        title: {
+            title: 'リマインダー: ON'
+        },
+        icons: {
+            path: {
+                '19': 'icons/19.png',
+                '38': 'icons/38.png'
+            }
+        }
+    };
 
-		if (isOff) {
-				settings.title = {
-						title: 'リマインダー: OFF'
-				};
-				settings.icons.path = {
-						'19': 'icons/19-off.png',
-						'38': 'icons/38-off.png'
-				};
-		}
+    if (isOff) {
+        settings.title = {
+            title: 'リマインダー: OFF'
+        };
+        settings.icons.path = {
+            '19': 'icons/19-off.png',
+            '38': 'icons/38-off.png'
+        };
+    }
 
-		chrome.browserAction.setTitle(settings.title);
-		chrome.browserAction.setIcon(settings.icons);
-		chrome.storage.sync.set({
-				isOff: isOff
-		});
+    chrome.browserAction.setTitle(settings.title);
+    chrome.browserAction.setIcon(settings.icons);
+    chrome.storage.sync.set({
+        isOff: isOff
+    });
 }
 
 chrome.storage.sync.get({
-		isOff: false
+    isOff: false
 }, function(data) {
-		isOff = data.isOff;
+    isOff = data.isOff;
 });
 
 chrome.browserAction.onClicked.addListener(toggleOnOff);
